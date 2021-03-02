3caba3137d8621f9dad2ba22283ca68c727bc396
Nascimento <vitor.c.nascimento@accenture.com>
2021-03-02 20:11:32 -0300

    Hello springBoot Udemy

---
+++ src/main/java/com/br/vitornascimento/StartUp.java
@@ -0,0 +1,14 @@
+package com.br.vitornascimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
