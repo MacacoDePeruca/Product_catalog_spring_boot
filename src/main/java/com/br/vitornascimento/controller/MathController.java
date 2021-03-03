0a3f21870317e582efa0370132019aceb7f49c50
Nascimento <vitor.c.nascimento@accenture.com>
2021-03-02 22:41:55 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Nascimento <vitor.c.nascimento@accenture.com>
    
     On branch master
     Your branch is ahead of 'origin/master' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   .gitignore
    	modified:   src/main/java/com/br/vitornascimento/controller/MathController.java
    
     Untracked files:
    	src/main/java/com/br/vitornascimento/OperationValidator.java
    	src/main/java/com/br/vitornascimento/Operations.java
    	src/main/java/com/br/vitornascimento/exception/

--- src/main/java/com/br/vitornascimento/controller/MathController.java
+++ src/main/java/com/br/vitornascimento/controller/MathController.java
@@ -1,20 +1,53 @@
-package com.br.vitornascimento;
+package com.br.vitornascimento.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class GreetingController {
+import com.br.vitornascimento.Operations;
 
-private static final String template = "Hello, %s!";
-	
-	private final AtomicLong counter = new AtomicLong();
-	
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));		
+@RestController
+public class MathController {
+
+	@RequestMapping("/sum/{numberOne}/{numberTwo}")
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+
+		return Operations.sum(numberOne, numberTwo);
 	}
+
+	@RequestMapping("/minus/{numberOne}/{numberTwo}")
+	public Double minus(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+
+		return Operations.subtraction(numberOne, numberTwo);
+
+	}
+
+	@RequestMapping("/multply/{numberOne}/{numberTwo}")
+	public Double multply(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+
+		return Operations.multiplication(numberOne, numberTwo);
+
+	}
+
+	@RequestMapping("/share/{numberOne}/{numberTwo}")
+	public Double share(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+
+		return Operations.division(numberOne, numberTwo);
+
+	}
+
+	@RequestMapping("/average/{numberOne}/{numberTwo}")
+	public Double average(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+
+		return Operations.mean(numberOne, numberTwo);
+
+	}
+
+	@RequestMapping("/root/{numberOne}")
+	public Double root(@PathVariable("numberOne") String numberOne) {
+
+		return Operations.squareRoot(numberOne);
+
+	}
+
 }
